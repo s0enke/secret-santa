f3dfcc6f018a6d6845ed4e0e5ab7c5824c65c03e
Soenke Ruempler <soenke@jimdo.com>
2014-12-12 19:09:23 +0100

    Initial version:
     - Setup via Ansible and CloudFormation
     - Notfier via Python/AWS Lambda
     - Data backend via DynamoDB

---
+++ README.md
@@ -0,0 +1,78 @@
+# Secret Santa
+
+This is a playground project in order to learn "future technologies" like
+serverless/containerless applicaions with AWS Lambda etc. and some mathematical
+theory.
+
+It's a simple machine which creates a "Secret Santa" list and sends out mails to
+participants.
+
+## Setup
+
+All setup (except SES outgoing mail) is automated with Ansible. It will use CloudFormation where possible.
+
+### Deploy everything
+
+```
+source setup.sh
+ansible-playbook deploy.yml
+```
+
+### Deploy fixtures to DynamoDB
+
+Create a `fixtures.yml` looking like this:
+
+```
+---
+user:
+  - PutRequest:
+      Item:
+        id:
+          S: soenke
+  - PutRequest:
+      Item:
+        id:
+          S: bernd
+        blacklist:
+          L:
+            - S: werner
+  - PutRequest:
+      Item:
+        id:
+          S: werner
+        blacklist:
+          L:
+            - S: bernd
+  - PutRequest:
+      Item:
+        id:
+          S: horst
+        blacklist:
+          L:
+            - S: werner
+            - S: soenke
+```
+
+Populate DynamoDB table:
+
+```
+source setup.sh
+ansible-playbook load-fixtures.yml
+```
+
+## TODO
+
+ - consistent naming
+ - local development env
+ - tests for the notifier
+ - give it a nice GUI e.g. with API Gateway, React and co
+ - Auth with Cognito/OIDC
+ - make it entirely AWS region agnostic
+ - Learn more theory (permutations, derangement etc.) and apply more mathematical terms
+ - store and remember last "draws" (Ziehungen)
+
+## Resources
+
+### Mathematical background
+
+ - https://en.wikipedia.org/wiki/Derangement
---
+++ julklapp.py
@@ -0,0 +1,109 @@
+import sys
+import itertools
+import random
+import copy
+import boto3
+from collections import defaultdict
+
+def lambda_handler(event, context):
+
+    dynamodb = boto3.client('dynamodb')
+
+    wichtel_result = dynamodb.scan(
+            TableName='user',
+    )
+
+    wichtel = []
+    blacklist = {}
+
+    for user in wichtel_result['Items']:
+        user_id = user['id']['S']
+        wichtel.append(user_id)
+        print user['blacklist']['L']
+        blacklist[user_id] = [blacklist_item['S'] for blacklist_item in user['blacklist']['L']]
+
+# tuple permutations
+    l = itertools.permutations(wichtel, 2)
+    d = defaultdict(list)
+    for v,k in l:
+      d[k].append(v)
+
+# blacklist anwenden
+    possible_donations = {}
+    for donor,receivers in d.items():
+      a = set(receivers) - set(blacklist[donor])
+      possible_donations[donor] =  list(a)
+    print possible_donations
+
+# integrity check (no empty possibilities)
+
+# integrety check 2: no uniq receiver
+
+# choose
+    donations = {}
+    for donor in d:
+      print '------------'
+      print donations
+      print donor, possible_donations[donor]
+
+      while True:
+
+        receiver = random.choice(possible_donations[donor])
+
+        new_possible_donations = copy.deepcopy(possible_donations)
+
+#        print '===================='
+#        print "possible donations: ", new_possible_donations
+#        print "random choice for donor %s: %s" % (donor, receiver)
+
+        del new_possible_donations[donor]
+        found_empty_receiver = False
+        for remaining_donor, remaining_receivers in new_possible_donations.items():
+            try:
+                new_possible_donations[remaining_donor].remove(receiver)
+            except ValueError:
+                pass
+
+            if not len(new_possible_donations[remaining_donor]):
+                print "empty remaining donor %s while choosing receiver for %s" % (remaining_donor, donor)
+                found_empty_receiver = True
+                break
+
+        if found_empty_receiver:
+            print "broken donations list (needs retry): ", new_possible_donations
+            continue
+
+        # TODO: check for deadlocks here, there MUST NOT be any equal lists
+
+        possible_donations = new_possible_donations
+        donations[donor] = receiver
+        break
+
+    print donations
+    return
+
+    session = boto3.session.Session(region_name='us-east-1')
+    client = session.client('ses')
+
+
+    for donor,receiver in donations.items():
+        to_email = '%s@ruempler.eu' % receiver
+        response = client.send_email(
+                Source='Die Wichtelwuerfelmaschine <soenke@ruempler.eu>',
+                Destination={
+                    'ToAddresses': [
+                        'soenke@ruempler.eu',
+                        ],
+                    },
+                Message={
+                    'Subject': {
+
+                        'Data': 'Deine Wichtelwahl',
+                        },
+                    'Body': {
+                        'Text': {
+                            'Data': "Hallo %s!\n\nDieses Jahr darfst du %s ein Geschenk machen! Gruss, deine automatische Wichtelmaschine." % (donor, receiver),
+                            },
+                        }
+                    },
+                )
